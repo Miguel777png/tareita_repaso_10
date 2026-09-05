7eee5c385c41c51670602eb29dea47065350cda1
Miguel777png <mcastillonorambuena2@gmail.com>
2026-09-05 09:36:30 -0400

    añadiendo recoleccion de datos

--- app/src/main/java/com/example/tareatest/PublicarObjetoActivity.java
+++ app/src/main/java/com/example/tareatest/PublicarObjetoActivity.java
@@ -3,6 +3,10 @@ package com.example.tareatest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +17,13 @@ import androidx.core.view.WindowInsetsCompat;
 public class PublicarObjetoActivity extends AppCompatActivity {
 
 
-    private Button btnBack;
+    private Button btnBack ,button;
+
+    private EditText txtNombre;
+    private RadioGroup GrupoRadio;
+    private Integer IdSeleccionado;
+
+    private String nombre , categoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,9 @@ public class PublicarObjetoActivity extends AppCompatActivity {
             return insets;
         });
 
+
+        txtNombre = findViewById(R.id.txtNombre);
+
         btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(v -> {
             Intent intent = new Intent(PublicarObjetoActivity.this, MenuPrincipalActivity.class);
@@ -35,6 +48,33 @@ public class PublicarObjetoActivity extends AppCompatActivity {
         });
 
 
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+
+            nombre = txtNombre.getEditableText().toString();
+            if (nombre.isEmpty()){
+
+                txtNombre.setError("El nombre es obligatorio!");
+            }
+
+
+
+
+            IdSeleccionado = GrupoRadio.getCheckedRadioButtonId();
+            if(IdSeleccionado != -1){
+
+                RadioButton ObtenerRB = findViewById(IdSeleccionado);
+                categoria = ObtenerRB.getText().toString();
+
+
+            }else{
+
+                Toast.makeText(this, "Por favor, selecciona una opción de categoria", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
 
     }
 }
